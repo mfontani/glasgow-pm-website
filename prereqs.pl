50bdfe0083316afa4edecd65e2a0707cf84d4e49
Marco Fontani <mfontani@cpan.org>
2010-10-29 22:32:58 +0100

    Date::Calc as prereq

--- prereqs.pl
+++ prereqs.pl
@@ -7,7 +7,7 @@ sub warn_if_not_found {
     if ( $@ ) { push @not_found, $module } else { push @found, $module }
 }
 
-warn_if_not_found $_ for qw/Text::Xslate/;
+warn_if_not_found $_ for qw/Text::Xslate Date::Calc/;
 
 if (@found)     { print "### Prerequisites found:\n";     print "# $_\n" for @found };
 if (@not_found) { print "### Prerequisites not found:\n"; print "$_\n"   for @not_found; }
